9c4379da614a04292eb0df60f3864dd6f4c703b5
Adrian Fernandes <boybothere@gmail.com>
2025-10-19 19:37:04 +0530

    add schema information in systme prompt and add specific instructions for better response and message handling

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,6 @@
+import { db } from '@/db/db';
 import { openai } from '@ai-sdk/openai';
-import { streamText, UIMessage, convertToModelMessages, tool } from 'ai';
+import { streamText, UIMessage, convertToModelMessages, tool, stepCountIs } from 'ai';
 import { z } from "zod";
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -8,13 +9,16 @@ export async function POST(req: Request) {
     const { messages }: { messages: UIMessage[] } = await req.json();
 
     const SYSTEM_PROMPT = `You are an expert SQL assistant that helps users to query their database using natural language.
-
+${new Date().toLocaleString('sv-SE')}
 You have access to following tools:
-1. db tool - call this tool to query the database.
+1. schema tool - call this to get the database schema which will help you to write sql query
+2. db tool - call this tool to query the database.
 
 Rules:
 - Generate ONLY SELECT queries (no INSERT, UPDATE, DELETE, DROP)
-- Return valid SQLite syntax
+- Always use the schema provided by the schema tool
+- Pass in valid SQL syntax in DB tool
+- IMPORTANT : To query database call DB tool, don't just return SQL query
 
 Always respond in a helpful, conversational tone while being technically accurate.`;
 
@@ -22,16 +26,44 @@ Always respond in a helpful, conversational tone while being technically accurat
         model: openai('gpt-4o-mini'),
         messages: convertToModelMessages(messages),
         system: SYSTEM_PROMPT,
+        stopWhen: stepCountIs(5),
         tools: {
+            schema: tool({
+                description: 'Call this tool to get database schema information',
+                inputSchema: z.object({}),
+                execute: async () => {
+                    return `CREATE TABLE products (
+	id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	name text NOT NULL,
+	category text NOT NULL,
+	price real NOT NULL,
+	stock integer DEFAULT 0 NOT NULL,
+	created_at text DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE sales (
+	id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	product_id integer NOT NULL,
+	quantity integer NOT NULL,
+	total_amount real NOT NULL,
+	sale_date text DEFAULT CURRENT_TIMESTAMP,
+	customer_name text NOT NULL,
+	region text NOT NULL,
+	FOREIGN KEY (product_id) REFERENCES products(id) ON UPDATE no action ON DELETE no action
+);
+
+                `;
+                },
+            }),
             db: tool({
                 description: 'Call this tool to query a database',
                 inputSchema: z.object({
                     query: z.string().describe('The SQL query to be ran.'),
                 }),
+
                 execute: async ({ query }) => {
                     console.log("Query: ", query);
-
-                    return '';
+                    return await db.run(query);
                 },
             }),
         },
